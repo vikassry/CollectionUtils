824f6914b21d1043ab8231526eb159f3d339f7d2
Vikas <=>
2015-02-28 12:33:01 +0530

    filter done for giving even numbers

--- Collection.java
+++ Collection.java
@@ -3,34 +3,51 @@ import java.util.ArrayList;
 
 
 interface ListMapper{
-	public List<Integer> GiveList(List<Integer> list);
+	public List<Integer> giveList(List<Integer> list);
+}
+
+interface ListFilter{
+	public List<Integer> giveEven(List<Integer> list);
 }
 
 class Mapper implements ListMapper{
-	public List<Integer> GiveList(List<Integer> list){
-		List<Integer> l = new ArrayList<Integer>();
+	public List<Integer> giveList(List<Integer> list){
+		List<Integer> new_list = new ArrayList<Integer>();
 		for (Integer i:list) {
-			l.add(i+1);
+			new_list.add(i+1);
 		}
-		return l;	
+		return new_list;	
+	}
+}
+
+class Filter implements ListFilter{
+	public List<Integer> giveEven(List<Integer> list){
+		List<Integer> filtered_list = new ArrayList<Integer>();
+		for (Integer i:list) {
+			if(i%2==0) filtered_list.add(i);
+		}
+		return filtered_list;
 	}
 }
 
 class CollectionUtils{
 	public static List<Integer> map(List<Integer> list, ListMapper mapper){
-		return mapper.GiveList(list);
+		return mapper.giveList(list);
+	}
+	public static List<Integer> filter(List<Integer> list, ListFilter filter){
+		return filter.giveEven(list);
 	}
 }
 
 public class Collection{
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<Integer>();
-		ListMapper lm = new Mapper();
+		ListFilter lm = new Filter();
 		list.add(1); 
 		list.add(2); 
 		list.add(3);
 		list.add(4);
-		List<Integer> result = CollectionUtils.map(list, lm);
+		List<Integer> result = CollectionUtils.filter(list, lm);
 		for (Integer i : result) {
 			System.out.println(i);
 		}
--- CollectionUtilTest.java
+++ CollectionUtilTest.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class CollectionUtilTest{
 
 	@Test
-	public void map_returns_new_list_of_incremented_integers(){
+	public void map_returns_new_list_with_incremented_integerValues(){
 		List<Integer> list = new ArrayList<Integer>();
 		List<Integer> expected = new ArrayList<Integer>();
 		ListMapper map = new Mapper();
@@ -17,7 +17,7 @@ public class CollectionUtilTest{
 		for (int i=0; i<3; i++) 
 			assertEquals(expected.get(i), result.get(i));
 	}
-	
+
 }
 
 
